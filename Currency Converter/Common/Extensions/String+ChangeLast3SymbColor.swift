be64688809909284a412adf00a5331298ee9cd41
RuslanWorkGit <lulka758@gmail.com>
2024-10-13 10:54:13 +0300

    add extensions

---
+++ Currency Converter/Common/Extensions/String+ChangeLast3SymbColor.swift
@@ -0,0 +1,20 @@
+//
+//  String+ChangeLast3SymbColor.swift
+//  Currency Converter
+//
+//  Created by Ruslan Liulka on 08.08.2023.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    func createAttributedText() -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: self)
+        if self.count >= 3 {
+            let lastThreeRange = NSRange(location: self.count - 3, length: 3)
+            attributedString.addAttribute(.foregroundColor, value: UIColor.tabBarUnselected, range: lastThreeRange)
+        }
+        return attributedString
+    }
+}
